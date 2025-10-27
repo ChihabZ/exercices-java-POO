0566ba4cb4e18247cacdd2f5f2e021e678369bc9
chihab zaim <147888742+ChihabZ@users.noreply.github.com>
2025-10-27 15:31:49 +0100

    Implement HeureClassExo2 with time validation

--- TP-2/classes/exercice2-class-heure.java
+++ TP-2/classes/exercice2-class-heure.java
@@ -1,1 +1,73 @@
+package tp2;
+
+public class HeureClassExo2 {
+	 
+	private int  Heure, Minute, Seconde;
+	
+	
+	
+	private boolean verif(int Heure, int Minute , int Seconde) {
+		 
+		 if (Heure >= 23  && Heure < 0 || Minute>=60  && Minute<0 || Seconde >= 60 && Seconde < 0 ){
+		 
+			return false; 
+		 }
+			else {
+				return true;
+			}
+			 
+	 }
+	
+	public HeureClassExo2(int Heure,int  Minute,int Seconde) {
+		
+		if (verif(Heure, Minute , Seconde)){
+		
+		this.Seconde = Seconde;
+		this.Heure = Heure;
+		this.Minute = Minute;
+		
+		
+	}
+	
+		 
+}
+ 
+	
+	
+	void addSec(int sec) {
+		
+		int intermedH = this.Heure * 3600;
+		int intermedM = this.Minute * 60;
+		int intermedS = this.Seconde + sec;
+		
+		intermedS += intermedH + intermedM;
+		
+		this.Heure = intermedS / 3600;
+		if(this.Heure >23) {
+			this.Heure %= 24;
+		}
+		
+			this.Minute = intermedS / 60;
+			if(this.Minute > 59) {
+				this.Minute %= 60;
+			}
+			
+			this.Seconde = intermedS;
+			if (this.Seconde > 59) {
+				this.Seconde %= 60;
+			}
+			
+			
+		
+		
+		
+	}
+	
+	
+public	 void afficherHeure() {
+		
+		System.out.println(this.Heure+":" + this.Minute + ":" + this.Seconde);
+	}
+ 
+}
 
