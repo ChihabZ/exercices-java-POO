8cc83a3c67a95eac0a921ac26b7c24dd44362b71
chihab zaim <147888742+ChihabZ@users.noreply.github.com>
2025-10-27 15:29:49 +0100

    Add MainExo2 class with time manipulation

--- TP-2/mains/exercice2.java
+++ TP-2/mains/exercice2.java
@@ -1,1 +1,19 @@
+package tp2;
+
+public class MainExo2 {
+	
+	public static void main(String[] args) {
+
+	
+		HeureClassExo2  heure = new  HeureClassExo2(15,23,44);
+		
+		heure.afficherHeure();
+		
+		heure.addSec(125);
+		
+		heure.afficherHeure();
+		
+	}
+	
+}
 
